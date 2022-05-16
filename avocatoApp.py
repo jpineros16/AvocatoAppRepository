6fd3b2c28200690a018894273932934201dfe47b
jpineros16 <jpineros16@gmail.com>
2022-05-15 20:48:30 -0500

    Adding model

---
+++ avocatoApp.py
@@ -0,0 +1,69 @@
+import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.models import load_model
+import time
+import cv2
+from tensorflow.keras import preprocessing
+
+fig = plt.figure()
+
+with open("custom.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+st.title('Clasificador de madurez de aguacate')
+
+st.markdown("Bienvenido. Sube una imagen de un aguacate y te dirá en que estado de maduración se encuentra")
+
+
+def main():
+    file_uploaded = st.file_uploader("Escoge un archivo", type=["png","jpg","jpeg"])
+    class_btn = st.button("Clasificar")
+    if file_uploaded is not None:    
+        #image = Image.open(file_uploaded)
+        imageShow = Image.open(file_uploaded)
+        image = cv2.cvtColor(np.array(imageShow), cv2.COLOR_RGB2BGR)
+        st.image(imageShow, caption='Imagen Cargada', use_column_width=True)
+        
+    if class_btn:
+        if file_uploaded is None:
+            st.write("Comando inválido. Por favor sube una imagen")
+        else:
+            with st.spinner('Modelo trabajando...'):
+                #plt.imshow(image)
+                #plt.axis("off")
+                predictions = predict(image)
+                #time.sleep(1)
+                st.success('Clasificado')
+                st.write(predictions)
+                #st.pyplot(fig)
+
+
+def predict(image):
+    classifier_model = "simple_NN_model.h5"
+    model = load_model(classifier_model, compile=False)#, custom_objects={'KerasLayer': hub.KerasLayer})
+    #test_image = image.resize((24,32))
+    test_image = cv2.resize(image, (24,32))
+    test_image = preprocessing.image.img_to_array(test_image)
+    test_image = test_image/ 255.0
+
+    # for a simple fully-connected network, flatten the image
+    test_image = test_image.flatten()
+    test_image = np.expand_dims(test_image, axis=0)
+    
+    class_names = [
+          'Maduro',
+          'Sobremaduro',
+          'Verde']
+    predictions = model.predict(test_image)
+    scores = predictions
+
+   
+    result = f"{class_names[np.argmax(scores)]} with a { (100 * np.max(scores)).round(2) } % confidence." 
+    return result
+
+
+
+if __name__ == "__main__":
+    main()
